fb8591e143e38a2e427a3e5d23a4341e4d8a365d
rookiemonkey <kevinroirigorbasina@pm.me>
2024-09-27 20:28:23 +0800

    move react fe to index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # expose the react fe
-  get "spa", to: "spa#index"
+  get "/", to: "spa#index"
   
   # expose apis
   post "api/send_messages", to: "api#send_messages"
